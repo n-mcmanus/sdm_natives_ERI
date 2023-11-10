4bd559490bf645cb44e47a74638071765dd1127c
n-mcmanus <nmcmanus@bren.ucsb.edu>
2023-11-10 12:45:20 -0800

    updating to NATSGO data and testing for soil depth affect on model

--- R/env_extract.R
+++ R/env_extract.R
@@ -14,16 +14,17 @@
 #' @param endYear character or numeric. The last water year in dataset.
 #' @param pathMonth character. File path to directory with monthly rasters.
 #' @param pathQuarter character. File path to directory with quarterly data (generated from `quarterly_rast()` function).
-#' @param pathSoil character. File path to directory with SSURGO soil data.
-#' @param sppOcc data frame. Contains species occurrence (or background points) data for extraction.
-#' @param lon character. Variable name for longitude values in sppOcc df (default = "lon")
-#' @param lat character. Variable name for the latitude values in sppOcc df (default = "lat")
+#' @param soilRast raster. NATSGO mapunit raster.  
+#' @param horizon data frame. Summarized soil horizon data to each map unit.
+#' @param occ data frame. Contains species occurrence or background points data for extraction.
+#' @param lon character. Variable name for longitude values in occ df (default = "lon")
+#' @param lat character. Variable name for the latitude values in occ df (default = "lat")
 #' @param crs character. The coordinate reference system of the spp occurrence data (default = "WGS84")
 #' @return data frame with environmental values at point of species observation
 
 
-env_extract <- function(startYear, endYear, pathMonth, pathQuarter, pathSoil,
-                        sppOcc, lon = "lon", lat = "lat", crs = "WGS84") {
+env_extract <- function(startYear, endYear, pathMonth, pathQuarter, soilRast, horizon,
+                        occ, lon = "lon", lat = "lat", crs = "WGS84") {
   
   ## Warnings
   if (startYear < 2000)
@@ -58,89 +59,93 @@ env_extract <- function(startYear, endYear, pathMonth, pathQuarter, pathSoil,
   ## BCM Extract loop --------------------------------------------------
   ## empty df to store loop results
   bcmExtract_df <- data.frame()
-  
-  ## LOOP START 
-  for (i in 1:nrow(dates_df)) {
-    ## List of monthly raster files
-    filesMonth <- list.files(path = pathMonth, 
-                        ## only list those with matching yr/mo in name
-                        pattern= paste0(dates_df$year[i], dates_df$mon[i]), 
-                        full.names=F)
     
-    ## List of quarterly raster files
-    filesQuarter <- list.files(path = pathQuarter,
-                               pattern = paste0(dates_df$wy[i]),
-                               full.names = F)
-    
-    
-    ## Stack all rasters
-    env_stack <- terra::rast(c(paste0(pathMonth,filesMonth), 
-                               paste0(pathQuarter,filesQuarter)))
-    
-    ## Rename to get unique layers for quarter rasts (doesn't read in _ correct)
-    names(env_stack) <- c((gsub(".tif", "", filesMonth)), (gsub(".tif", "", filesQuarter)))
-    
-    ## Filter obs to yr/mo
-    sppOcc_filter <- sppOcc %>% 
-      filter(year == dates_df$year[i],
-             month == dates_df$mon_num[i])
-    
-    ## If filtered df has obs, then vectorize and extract.
-    if(nrow(sppOcc_filter) > 0) {
-        ## vectorize and reproj to env data crs
-        sppOcc_vect <- sppOcc_filter %>%
-          terra::vect(geom = c(lon, lat), crs = crs) %>%
-          terra::project(y = crs(env_stack))
-        
-        ## extract and tidy df
-        sppExtract <- extract(env_stack, sppOcc_vect, method = "simple") %>%
-          dplyr::select(-ID) %>% 
-          ## only keep first 3 chars of monthly columns
-          ## (e.g. "cwd2021jan" becomes "cwd")
-          rename_with(.fn= ~substr(., 1, 3), 
-                      .cols = 1:(nlyr(env_stack)-3)) %>% 
-          ## rename last three rows (quarterly rasts)
-          ## (e.g. "ppt2021winter_mean" becomes "ppt_winter_mean")
-          rename_with(.fn= ~gsub(dates_df$wy[i], "_", x=.), 
-                      .cols = (ncol(.)-2):(ncol(.))) %>% 
-          ## merge occ data w/extract data
-          cbind(sppOcc_filter, .)
-        
-        ## append results to df
-        bcmExtract_df <- rbind(bcmExtract_df, sppExtract)
+    ## LOOP START 
+    for (i in 1:nrow(dates_df)) {
+      ## List of monthly raster files
+      filesMonth <- list.files(path = pathMonth, 
+                          ## only list those with matching yr/mo in name
+                          pattern= paste0(dates_df$year[i], dates_df$mon[i]), 
+                          full.names=F)
       
-    } else {
-      ## If no obs for a yr/mo, skip
-      next
-    } ### END if/else statement
+      ## List of quarterly raster files
+      filesQuarter <- list.files(path = pathQuarter,
+                                 pattern = paste0(dates_df$wy[i]),
+                                 full.names = F)
+      
+      
+      ## Stack all rasters
+      env_stack <- terra::rast(c(paste0(pathMonth,filesMonth), 
+                                 paste0(pathQuarter,filesQuarter)))
+      
+      ## Rename to get unique layers for quarter rasts (doesn't read in _ correct)
+      names(env_stack) <- c((gsub(".tif", "", filesMonth)), 
+                            (gsub(".tif", "", filesQuarter)))
+      
+      ## Filter obs to yr/mo
+      occ_filter <- occ %>% 
+        filter(year == dates_df$year[i],
+               month == dates_df$mon_num[i])
+      
+      ## If filtered df has obs, then vectorize and extract.
+      if(nrow(occ_filter) > 0) {
+          ## vectorize and reproj to env data crs
+          occ_vect <- occ_filter %>%
+            terra::vect(geom = c(lon, lat), crs = crs) %>%
+            terra::project(., y = crs(env_stack))
+          
+          ## extract and tidy df
+          sppExtract <- extract(env_stack, occ_vect, 
+                                method = "simple", ID = FALSE) %>%
+            ## only keep first 3 chars of monthly columns
+            ## (e.g. "cwd2021jan" becomes "cwd")
+            rename_with(.fn= ~substr(., 1, 3), 
+                        .cols = 1:(nlyr(env_stack)-3)) %>% 
+            ## rename last three rows (quarterly rasts)
+            ## (e.g. "ppt2021winter_mean" becomes "ppt_winter_mean")
+            rename_with(.fn= ~gsub(dates_df$wy[i], "_", x=.), 
+                        .cols = (ncol(.)-2):(ncol(.))) %>% 
+            ## merge occ data w/extract data
+            cbind(occ_filter, .)
+          
+          ## append results to df
+          bcmExtract_df <- rbind(bcmExtract_df, sppExtract)
+        
+      } else {
+        ## If no obs for a yr/mo, skip
+        next
+      } ### END if/else statement
+      
+      setTxtProgressBar(pb, i)
+      
+    } ### END LOOP
     
-    setTxtProgressBar(pb, i)
-    
-  } ### END LOOP
-  
   
   ## Extract for soil ------------------------------------------
+    
+    ## Vectorize occurrence data so far
+    occ_vect <- bcmExtract_df %>%
+      terra::vect(geom = c(lon, lat), crs = crs) %>%
+      terra::project(y = crs(soilRast))
+
+    ## Extract mapunit key for each point
+    soilExtract_df <- extract(soilRast, occ_vect,
+                          method = "simple", ID = FALSE) %>%
+      cbind(bcmExtract_df, .) %>%
+      janitor::clean_names() %>%
+      mutate(mukey = as.character(mukey))
+
+    ## Ensure horizon mukey same class as extract_df
+    horizon <- horizon %>%
+      mutate(mukey = as.character(mukey))
+
+    ## Join soil data w/occ by mapunit
+    extract_df <- left_join(soilExtract_df, horizon, by = "mukey")
+      
+
   
-  ## List of soil raster files
-  filesSoil <- list.files(path = pathSoil,
-                          pattern = "_270m.tif",
-                          full.names = TRUE)
-  ## Stack them
-  soil_stack = terra::rast(c(filesSoil))
   
-  ## Vectorize
-  sppOcc_vect <- bcmExtract_df %>%
-    terra::vect(geom = c(lon, lat), crs = crs) %>%
-    terra::project(y = crs(soil_stack))
-  
-  ## Extract
-  extract_df <- extract(soil_stack, sppOcc_vect, method = "simple") %>% 
-    dplyr::select(-ID) %>% 
-    ## merge occ data w/extract data
-    cbind(bcmExtract_df, .)
-  
-  
-    return(extract_df)
+  return(extract_df)
   
   
 } ### end fxn
