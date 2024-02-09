6846a5024b1ae3c92822174a044c2f7c0821a777
nmcmanus <nmcmanus@bren.ucsb.edu>
2024-02-08 17:59:05 -0800

    added more annotations

--- R/env_extract.R
+++ R/env_extract.R
@@ -39,6 +39,7 @@ env_extract <- function(startYear, endYear, pathMonth, pathQuarter, pathSoil,
                          mon = c('oct', 'nov', 'dec', 'jan', 'feb', 'mar', 
                                  'apr', 'may', 'jun', 'jul', 'aug', 'sep'),
                          mon_num = rep(c(10:12, 1:9), each=1)) %>% 
+    ## generate calendar year from wy 
     mutate(year = case_when(mon_num %in% c(10, 11, 12) ~(.$wy-1),
                             .default = .$wy))
   
@@ -56,33 +57,41 @@ env_extract <- function(startYear, endYear, pathMonth, pathQuarter, pathSoil,
   extract_df <- data.frame()
     
     ## LOOP START 
+    ## run through every month of time period
     for (i in 1:nrow(dates_df)) {
-      ## List of monthly raster files
-      filesMonth <- list.files(path = pathMonth, 
-                          ## only list those with matching yr/mo in name
-                          pattern= paste0(dates_df$year[i], dates_df$mon[i]), 
-                          full.names=TRUE)
       
-      ## List of quarterly raster files
-      filesQuarter <- list.files(path = pathQuarter,
-                                 pattern = paste0(dates_df$wy[i]),
-                                 full.names = TRUE)
-      ## Soil properties
-      filesSoil <- list.files(path = pathSoil,
-                              pattern = paste0("natsgo_", ".+", ".tif"),
-                              full.names = TRUE)
-      
-      ## Stack all rasters
-      env_stack <- terra::rast(c(filesMonth, filesQuarter, filesSoil))
-      
-      ## Filter obs to yr/mo
+      ## Filter obs to mo/yr
       occ_filter <- occ %>% 
         filter(year == dates_df$year[i],
                month == dates_df$mon_num[i])
       
-      ## If filtered df has obs, then vectorize and extract.
+      ## If filtered df has obs for that month, 
+      ## then read in env data, vectorize occ data, and extract.
       if(nrow(occ_filter) > 0) {
-          ## vectorize and reproj to env data crs
+          ## List of monthly raster files
+          filesMonth <- list.files(
+            path = pathMonth,
+            ## only list those with matching yr/mo in name
+            pattern = paste0(dates_df$year[i], dates_df$mon[i]),
+            full.names = TRUE
+          )
+          ## List of quarterly raster files
+          filesQuarter <- list.files(
+            path = pathQuarter,
+            pattern = paste0(dates_df$wy[i]),
+            full.names = TRUE
+          )
+          ## Soil properties
+          filesSoil <- list.files(
+            path = pathSoil,
+            pattern = paste0("natsgo_", ".+", ".tif"),
+            full.names = TRUE
+          )
+          ## Stack all rasters
+          env_stack <-
+            terra::rast(c(filesMonth, filesQuarter, filesSoil))
+        
+          ## vectorize and reproject to env data crs
           occ_vect <- occ_filter %>%
             terra::vect(geom = c(lon, lat), crs = crs) %>%
             terra::project(., y = crs(env_stack))
@@ -90,24 +99,24 @@ env_extract <- function(startYear, endYear, pathMonth, pathQuarter, pathSoil,
           ## extract and tidy df
           sppExtract <- extract(env_stack, occ_vect, 
                                 method = "simple", ID = FALSE) %>% 
-            ## only keep first 3 chars of monthly columns
-            ## (e.g. "cwd2021jan" becomes "cwd")
-            rename_with(.fn= ~substr(., 1, 3), 
-                        ## only for columns of monthly variables
-                        .cols = 1:length(filesMonth)) %>% 
-            ## rename quarterly rasts; replace wy with _
-            ## (e.g. "ppt2021winter_mean" becomes "ppt_winter_mean")
-            rename_with(.fn= ~gsub(dates_df$wy[i], "_", x=.), 
-                        ## only for columns of quarterly variables
-                        .cols = (length(filesMonth)+1):(ncol(.)-length(filesSoil))) %>% 
-            ## merge occ data w/extract data
-            cbind(occ_filter, .)
+              ## only keep first 3 chars of monthly columns
+              ## (e.g. "cwd2021jan" becomes "cwd")
+              rename_with(.fn= ~substr(., 1, 3), 
+                          ## only for columns of monthly variables
+                          .cols = 1:length(filesMonth)) %>% 
+              ## rename quarterly rasts; replace wy with _
+              ## (e.g. "ppt2021winter_mean" becomes "ppt_winter_mean")
+              rename_with(.fn= ~gsub(dates_df$wy[i], "_", x=.), 
+                          ## only for columns of quarterly variables
+                          .cols = (length(filesMonth)+1):(ncol(.)-length(filesSoil))) %>% 
+              ## merge occ data w/extract data
+              cbind(occ_filter, .)
           
           ## append results to df
           extract_df <- rbind(extract_df, sppExtract)
-        
+          
+      ## If no obs for a yr/mo, skip  
       } else {
-        ## If no obs for a yr/mo, skip
         next
       } ### END if/else statement
       
@@ -115,8 +124,6 @@ env_extract <- function(startYear, endYear, pathMonth, pathQuarter, pathSoil,
       
     } ### END LOOP
 
-
-  
   return(extract_df)
   
 } ### end fxn
