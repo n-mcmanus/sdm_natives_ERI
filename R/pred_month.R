84c3ddf4327bd653fdeedddf71c6c079073a3a71
nmcmanus <nmcmanus@bren.ucsb.edu>
2024-02-15 09:07:08 -0800

    update file naming convention for future predictions

--- R/pred_month.R
+++ R/pred_month.R
@@ -7,13 +7,15 @@
 #' 
 #' @author Nick McManus
 #' @param model the Maxent model used for predicting suitability
+#' @param spp the species being modeled
+#' @param model_years the model and time range of supplied env variables. This is only used for file naming convention, so best to include future model with years for clarity (e.g. "2000_2022" or "MIROC45_2070_2099"). 
 #' @param bcmPath path to directory of bcm monthly average rasters
 #' @param soilPath path to directory of soil rasters
 #' @param pathOut path to output directory where new rasters will be saved
 #' @return 12 TIFs with predicted habitat suitability by month
 
 
-pred_month <- function(model, bcmPath, soilPath, pathOut) {
+pred_month <- function(model, spp, model_years, bcmPath, soilPath, pathOut) {
   
   ## List of months for map
   month <- c('jan', 'feb', 'mar', 'apr', 'may', 'jun',
@@ -21,7 +23,7 @@ pred_month <- function(model, bcmPath, soilPath, pathOut) {
   
   
   ## Monthly pred fxn --------------------------------------------------------
-  pred_fxn <- function(model, month, bcmPath, soilPath, pathOut) {
+  pred_fxn <- function(model, month, model_years, bcmPath, soilPath, pathOut) {
       ## Read each variable
       aet <- raster::raster(list.files(path = bcmPath,
                                        pattern = paste0("aet", ".+", month),
@@ -32,8 +34,18 @@ pred_month <- function(model, bcmPath, soilPath, pathOut) {
       tdiff <- raster::raster(list.files(path = bcmPath,
                                          pattern = paste0("tdiff",".+", month),
                                          full = TRUE)) 
-      tmxSummer <- raster::raster(paste0(bcmPath, "tmx_summer_avg.tif"))
-      pptWinter <- raster::raster(paste0(bcmPath, "ppt_winter_avg.tif"))
+      # cwd <- raster::raster(list.files(path = bcmPath,
+      #                                    pattern = paste0("cwd",".+", month),
+      #                                    full = TRUE)) 
+      # ppt <- raster::raster(list.files(path = bcmPath,
+      #                                    pattern = paste0("ppt",".+", month),
+      #                                    full = TRUE)) 
+      tmxSummer <- raster::raster(list.files(path = bcmPath, 
+                                             pattern = "tmx_summer",
+                                             full = TRUE))
+      pptWinter <- raster::raster(list.files(path = bcmPath, 
+                                             pattern = "ppt_winter",
+                                             full = TRUE))
       om <- raster::raster(list.files(path = soilPath,
                                       pattern = "_om_",
                                       full = TRUE))
@@ -43,19 +55,22 @@ pred_month <- function(model, bcmPath, soilPath, pathOut) {
       cec <- raster::raster(list.files(path = soilPath,
                                        pattern = "_cec_",
                                        full = TRUE))
+      drclass <- raster::raster(list.files(path = soilPath,
+                                           pattern = "_drclass_",
+                                           full = TRUE))
       
       ## Stack and match lyr names to model variables
-      stack <- raster::stack(aet, tdiff, tmx, tmxSummer, pptWinter, om, ph, cec)
+      stack <- raster::stack(aet, tdiff, tmx, tmxSummer, pptWinter, om, ph, cec, drclass)
       names(stack) <- c('aet','tdiff', 'tmx', 'tmx_summer_mean', 
-                        'ppt_winter_sum', 'om', 'ph', 'cec')
+                        'ppt_winter_sum', 'om', 'ph', 'cec', 'drclass')
       
       ## Predict suitability with model, then save raster
       pred <- dismo::predict(model, stack)
-      writeRaster(pred, paste0(pathOut, month, '_pred.tif'), overwrite=TRUE)
+      writeRaster(pred, paste0(pathOut, month, '_', spp, '_', model_years, '.tif'), overwrite=TRUE)
   } ### END `pred_fxn()`
   
   ## Iterate fxn over list of months 
-  purrr::map(month, pred_fxn, model=model, 
+  purrr::map(month, pred_fxn, model=model, model_years,
              bcmPath, soilPath, pathOut, .progress = TRUE)
   
 } ### END SCRIPT
